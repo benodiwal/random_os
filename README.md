7f4f592b32984d3b4e021de2c4ed7fedadb9627b
codygeek01 <s474996633@gmail.com>
2024-06-14 17:10:40 +0530

    so done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # random_os
 
+A minimal rust kernel build upon a freestanding rust binary [random](https://github.com/benodiwal/random)
+
+## Prerequisites:
+
+- `Installing Rust Nightly`:
+```sh
+rustup override set nightly
+```
+
+- `Confirming your rust release channel:`  
+```sh
+rustc --version
+```
+`The version number should contain -nightly at the end`.
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static RANDOM: &[u8] = b"Random OS";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in RANDOM.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 } 
